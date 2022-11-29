7d457da3599ac10a33ea60923096d6fcb7e6cbfc
Tang9696 <95304937+Tang9696@users.noreply.github.com>
2022-11-29 16:37:51 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_1BftiY421', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4p10dee05jgtlsahnitkm1pu27', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'US East (N. Virginia)' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
